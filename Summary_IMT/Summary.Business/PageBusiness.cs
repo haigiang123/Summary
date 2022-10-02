49518a1363bc796df30b6ac65b18183cd77126f0
haigt <giangthanhhai17@gmail.com>
2022-10-02 22:27:23 +0700

    business

---
+++ Summary_IMT/Summary.Business/PageBusiness.cs
@@ -0,0 +1,34 @@
+﻿using Summary.Data.Infrastructure;
+using Summary.Data.Repositories;
+using Summary_IMT.Summary.Model.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summary.Business
+{
+    public interface IPageBusiness
+    {
+        Page GetByAlias(string alias);
+    }
+
+    public class PageBusiness : IPageBusiness
+    {
+        private IUnitOfWork _unitOfWork;
+        private IPageRepository _pageRepository;
+
+        public PageBusiness(IUnitOfWork unitOfWork, IPageRepository pageRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _pageRepository = pageRepository;
+        }
+
+        public Page GetByAlias(string alias)
+        {
+            return _pageRepository.GetSingleByCondition(x => x.Alias == alias);
+        }
+    }
+}
---
+++ Summary_IMT/Summary.Business/PostBusiness.cs
@@ -0,0 +1,83 @@
+﻿using Summary.Data.Infrastructure;
+using Summary_IMT.Summary.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Repositories;
+
+namespace Summary.Business
+{
+    public interface IPostBusiness
+    {
+        void Add(Post post);
+
+        void Update(Post post);
+
+        void Delete(int id);
+
+        IEnumerable<Post> GetAll();
+
+        IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow);
+
+        Post GetById(int id);
+
+        IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow);
+
+        void SaveChanges();
+    }
+
+    public class PostBusiness : IPostBusiness
+    {
+        private UnitOfWork _unitOfWork;
+        private IPostRepository _postRepository;
+
+        public void Add(Post post)
+        {
+            _postRepository.Add(post);
+        }
+
+        public void Delete(int id)
+        {
+            _postRepository.Delete(id);
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            return _postRepository.GetAll(new string[] { "PostCategory" });
+        }
+
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.CategoryID == categoryId, out totalRow, page, pageSize, new string[] { "PostCategory" });
+        }
+
+        public IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetAllByTag(tag, page, pageSize, out totalRow);
+        }
+
+        public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+        }
+
+        public Post GetById(int id)
+        {
+            return _postRepository.GetSingleById(id);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(Post post)
+        {
+            _postRepository.Update(post);
+        }
+    }
+}
